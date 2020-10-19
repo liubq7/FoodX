9f8856ad2032771454cfa5e79029fe4383f64d3c
liubq7 <liubq97@gmail.com>
2020-10-19 21:47:32 +0800

    add order script

---
+++ Assets/Scripts/DoubleTap.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleTap : MonoBehaviour
+{
+    public GameObject Dish;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        for (var i = 0; i < Input.touchCount; ++i)
+        {
+            if (Input.GetTouch(i).phase == TouchPhase.Began)
+            {
+                if (Input.GetTouch(i).tapCount == 2)
+                {
+                    int n = Int32.Parse(Dish.name);
+                    Order(n);
+                }
+            }
+        }
+    }
+
+    public void Order(int n)
+    {
+        AddedList.SushiMenu[i] = true;
+    }
+}
