3bf1c80808e68e99fe7e82f7ff905953ee13bb2c
Renyi Yuan <ryyuan98@gmail.com>
2020-10-19 10:51:17 +0800

    add dish change script

---
+++ Assets/Scripts/BackHome.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackHome : MonoBehaviour
+{
+    public Button mButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = mButton.GetComponent<Button>();
+        btn.onClick.AddListener(OnPress);
+    }
+
+     void OnPress()
+    {
+        Application.LoadLevel(0);
+    }
+}
---
+++ Assets/Scripts/DishChange.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class DishChange : MonoBehaviour
+{
+    public GameObject[] Sushis;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        for (int i = 0; i < Sushis.Length; i++)
+        {
+            Sushis[i].SetActive(false);
+        }
+;
+    }
+
+    public void SetDish(int v)
+    {
+        for (int i = 0; i < Sushis.Length; i++)
+        {
+            if (i == v)
+            {
+                Sushis[i].SetActive(true);
+            }
+            else
+            {
+                Sushis[i].SetActive(false);
+            }
+        }
+        throw new NotImplementedException();
+    }
+
+
+}
