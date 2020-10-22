8010c0871ad9f1e6c44d328c20e2d8c3c5180acf
Renyi Yuan <ryyuan98@gmail.com>
2020-10-22 18:12:03 +0800

    add price

---
+++ Assets/Scripts/TotalPrice.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TotalPrice : MonoBehaviour
+{
+    public Button mButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mButton.transform.Find("Text").GetComponent<Text>().text = "Ordered: " + System.Convert.ToString(DoubleTap.totalPrice);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
